72558de2ed4035a9278c11c8d3243da566e62689
wxlterj <waltermusicfernandez@gmail.com>
2023-11-07 19:57:04 -0500

    Configurando todo lo relacionado a Material Design

---
+++ app/src/main/java/com/example/a30diasdehabitos/ui/theme/Shape.kt
@@ -0,0 +1,9 @@
+package com.example.a30diasdehabitos.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    medium = RoundedCornerShape(12.dp)
+)
--- app/src/main/java/com/example/a30diasdehabitos/ui/theme/Type.kt
+++ app/src/main/java/com/example/a30diasdehabitos/ui/theme/Type.kt
@@ -2,18 +2,40 @@ package com.example.a30diasdehabitos.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.a30diasdehabitos.R
+
+
+val Oxygen = FontFamily(
+    Font(R.font.oxygen_regular, FontWeight.Normal),
+    Font(R.font.oxygen_bold, FontWeight.Bold),
+    Font(R.font.oxygen_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = Oxygen,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 30.sp
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Oxygen,
+        fontWeight = FontWeight.Light,
+        fontSize = 20.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Oxygen,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Oxygen,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp
     )
     /* Other default text styles to override
     titleLarge = TextStyle(
